daf95571f2d26d08e27e875437caf151b3b8cec4
renancunha <renancunhafonseca@hotmail.com>
2018-09-28 12:53:37 -0300

    Classe abstrata job criada

--- src/character/Job.java
+++ src/character/Job.java
@@ -1,4 +1,39 @@
 package character;
 
-public class Job {
+public abstract class Job {
+    private String name;
+
+    public abstract int setAtk(int dex, int sta, int str,
+                               int intel, int agi, int luk);
+
+    public abstract int setMAtk(int dex, int sta, int str,
+                               int intel, int agi, int luk);
+
+    public abstract int setDef(int dex, int sta, int str,
+                               int intel, int agi, int luk);
+
+    public abstract int setMDef(int dex, int sta, int str,
+                               int intel, int agi, int luk);
+
+    public abstract int setMaxHp(int dex, int sta, int str,
+                               int intel, int agi, int luk);
+
+    public abstract int setMaxSp(int dex, int sta, int str,
+                               int intel, int agi, int luk);
+
+    public abstract int setAtkSpd(int dex, int sta, int str,
+                               int intel, int agi, int luk);
+
+    public abstract int setEva(int dex, int sta, int str,
+                               int intel, int agi, int luk);
+
+    public abstract int setCri(int dex, int sta, int str,
+                               int intel, int agi, int luk);
+
+    public abstract int setHit(int dex, int sta, int str,
+                               int intel, int agi, int luk);
+
+    public abstract int setTen(int dex, int sta, int str,
+                               int intel, int agi, int luk);
+
 }
