d5b1d670a540e66fc8d51f67080e7f5d786d88ed
renancunha <renancunhafonseca@hotmail.com>
2018-09-28 12:44:36 -0300

    atributos atualizados com getter e setters

--- src/character/Character.java
+++ src/character/Character.java
@@ -7,8 +7,11 @@ public class Character {
   private String name;
   private Jobs job; //Define a classe do personagem por meio de um enumerador
 
+  //level
+  private int lvl;
+
   //atributos independentes
-  private int intel;
+  private int intelligence;
   private int strenght;
   private int stamina;
   private int agility;
@@ -23,48 +26,138 @@ public class Character {
   private int maxHp;
   private int maxSp;
   private int atkSpd;
-  private int tenacity;
+  private int ten;
   private int cri;
   private int hit;
   private int eva;
 
   //Construtor:
-  public Character(String name) {
-    this.job = Jobs.NOVICE; //Declara  job
+
+  public Character(String name, int lvl) {
     this.name = name;
-    this.stats = new Stats(job); //Passa o job para a instancia da classe stats
+    this.lvl = lvl;
     System.out.println("Personagem criado com sucesso!");
   }
 
+  public Character(String name) {
+    new Character(name,1);
+  }
 
   //Métodos auxiliares:
-  public boolean isDead() {
-    System.out.println(this.stats.get_status_value(TypeStats.HP));
-    if (this.stats.get_status_value(TypeStats.HP) > 0) return false;
-    return true;
+
+  public static int getN_characters() {
+    return n_characters;
   }
 
-  //Setters:
-  void set_name(String name){
-    this.name = name;
+  public String getName() {
+    return name;
   }
 
-  /*void set_stats(Stats stats){
-    this.stats = stats;
-  }*/
-
-  //Getters:
-  public String get_status() {
-    //Retorna algum tipo de dado com os status do personagem:
-    String stats = "not defined yet";
-    return stats; //precisamos definir como funionará este retorno
+  public Jobs getJob() {
+    return job;
   }
 
-  public String get_name(){
-    return this.name;
+  public int getLvl() {
+    return lvl;
   }
 
-  public Jobs get_job(){
-    return this.job;
+  public int getIntelligence() {
+    return intelligence;
+  }
+
+  public int getStrenght() {
+    return strenght;
+  }
+
+  public int getStamina() {
+    return stamina;
+  }
+
+  public int getAgility() {
+    return agility;
+  }
+
+  public int getDexterity() {
+    return dexterity;
+  }
+
+  public int getLuck() {
+    return luck;
+  }
+
+  public int getAtk() {
+    return atk;
+  }
+
+  public int getmAtk() {
+    return mAtk;
+  }
+
+  public int getDef() {
+    return def;
+  }
+
+  public int getmDef() {
+    return mDef;
+  }
+
+  public int getMaxHp() {
+    return maxHp;
+  }
+
+  public int getMaxSp() {
+    return maxSp;
+  }
+
+  public int getAtkSpd() {
+    return atkSpd;
+  }
+
+  public int getTen() {
+    return ten;
+  }
+
+  public int getCri() {
+    return cri;
+  }
+
+  public int getHit() {
+    return hit;
+  }
+
+  public int getEva() {
+    return eva;
+  }
+
+  public void setJob(Jobs job) {
+    this.job = job;
+  }
+
+  public void setLvl(int lvl) {
+    this.lvl = lvl;
+  }
+
+  public void setIntelligence(int intelligence) {
+    this.intelligence = intelligence;
+  }
+
+  public void setStrenght(int strenght) {
+    this.strenght = strenght;
+  }
+
+  public void setStamina(int stamina) {
+    this.stamina = stamina;
+  }
+
+  public void setAgility(int agility) {
+    this.agility = agility;
+  }
+
+  public void setDexterity(int dexterity) {
+    this.dexterity = dexterity;
+  }
+
+  public void setLuck(int luck) {
+    this.luck = luck;
   }
 }
