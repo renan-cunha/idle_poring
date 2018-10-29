4736b0c57cc3bd10d10d6d7c66a91e7a62c09c6d
Renan Cunha <renancunhafonseca@gmail.com>
2018-10-29 10:44:26 -0300

    "Class Attribute created"

---
+++ src/util/Attributes.java
@@ -0,0 +1,75 @@
+package util;
+
+public class Attributes {
+  private int dex; private int sta; private int str; private int intel;
+  private int agi; private int luk;
+
+  public Attributes(int dex, int sta, int str, int intel, int agi, int luk) {
+    this.dex = dex;
+    this.sta = sta;
+    this.str = str;
+    this.intel = intel;
+    this.agi = agi;
+    this.luk = luk;
+  }
+
+  @Override
+  public String toString() {
+    return "Attributes{" +
+            "dex=" + dex +
+            ", sta=" + sta +
+            ", str=" + str +
+            ", intel=" + intel +
+            ", agi=" + agi +
+            ", luk=" + luk +
+            '}';
+  }
+
+  public int getDex() {
+    return dex;
+  }
+
+  public void setDex(int dex) {
+    this.dex = dex;
+  }
+
+  public int getSta() {
+    return sta;
+  }
+
+  public void setSta(int sta) {
+    this.sta = sta;
+  }
+
+  public int getStr() {
+    return str;
+  }
+
+  public void setStr(int str) {
+    this.str = str;
+  }
+
+  public int getIntel() {
+    return intel;
+  }
+
+  public void setIntel(int intel) {
+    this.intel = intel;
+  }
+
+  public int getAgi() {
+    return agi;
+  }
+
+  public void setAgi(int agi) {
+    this.agi = agi;
+  }
+
+  public int getLuk() {
+    return luk;
+  }
+
+  public void setLuk(int luk) {
+    this.luk = luk;
+  }
+}
