caa59d605aaa48d2e7b08e375faa8683d575a419
Renan Cunha <renancunhafonseca@gmail.com>
2018-11-01 15:24:07 -0300

    Battle reseted

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,12 @@ public class Main {
 
     public static void main(String[] args) {
         //Criando um personagem (exemplo):
-        Character fulano = new Character("Fulano");
 
+
+        Character fulano = new Character("Fulano");
         Character ciclano = new Character("Ciclano");
 
-        Helmet capacete = new Helmet("HelmetXX",new Novice(),2,1,2,1,1,1,1);
+        Helmet capacete = new Helmet("HelmetXX",2,1,2,1,1,1,1);
         fulano.setJob(new Swordsman());
         Battle battle = new Battle(fulano, ciclano);
         battle.startBattle();
