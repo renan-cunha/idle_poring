866e245c12e4c10993d42bc267ab819fa1d96d3b
Renan Cunha <renancunhafonseca@gmail.com>
2018-12-03 16:57:06 -0300

    Metodo randomEquipmentType foi passado para o enum

--- src/item/EquipmentType.java
+++ src/item/EquipmentType.java
@@ -1,8 +1,17 @@
 package item;
 
 import java.util.LinkedList;
+import java.util.Random;
 
 public enum EquipmentType {
-  ACESSORY, ARMOR, FOOT_GEAR, HELMET, MANTEAU, WEAPON
+  ACESSORY, ARMOR, FOOT_GEAR, HELMET, MANTEAU, WEAPON;
 
+  public static EquipmentType randomEquipmentType(){
+    Random random = new Random();
+    EquipmentType[] equipmentTypes = EquipmentType.values();
+    int index = random.nextInt(equipmentTypes.length);
+    return equipmentTypes[index];
+  }
 }
+
+
