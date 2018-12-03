382a50c2dcf9ba19568dea1a463d898c317911e0
Renan Cunha <renancunhafonseca@gmail.com>
2018-12-03 17:58:33 -0300

    Metodo de soma de atributos refatorado

--- src/util/Attributes.java
+++ src/util/Attributes.java
@@ -12,20 +12,17 @@ public class Attributes {
   private int xp = 0;
   private static Random random = new Random();
 
-
   public Attributes() {
   }
 
   public Attributes(int level, int weight){
     this.level = level;
-
     this.dex = random.nextInt(level*weight)+1;
     this.sta = random.nextInt(level*weight)+1;
     this.str = random.nextInt(level*weight)+1;
     this.intel = random.nextInt(level*weight)+1;
     this.agi = random.nextInt(level*weight)+1;
     this.luk = random.nextInt(level*weight)+1;
-
   }
 
   public Attributes(int level, int dex, int sta, int str, int intel, int agi,
@@ -44,13 +41,12 @@ public class Attributes {
   }
 
   public void setXp(int xp) {
-    this.xp = xp;
-    if (this.xp >= 100) {
-      lvlUp();
-      this.xp = 0;
+    if(xp <0){
+      System.out.println("New xp is lower than 0");
+      System.exit(0);
     }
+    this.xp = xp;
   }
-
 
   public int getLevel() {
     return level;
@@ -58,7 +54,7 @@ public class Attributes {
 
   public void setLevel(int level) {
     if(level<=0){
-      System.out.println("New stage is lower or equal to 0");
+      System.out.println("New level is lower or equal to 0");
       System.exit(0);
     }
     this.level = level;
@@ -114,50 +110,16 @@ public class Attributes {
 
   public Attributes add(Attributes[] att){
 
-    Attributes result = new Attributes(getLevel(), getDex(), getSta(), getStr(),
-            getIntel(), getAgi(), getLuk());
+    Attributes result = new Attributes();
     for (int i = 0; i < att.length; i++){
-      int dex = result.getDex() + att[i].getDex();
-      int sta = result.getSta() + att[i].getSta();
-      int str = result.getStr() + att[i].getStr();
-      int agi = result.getAgi() + att[i].getAgi();
-      int luk = result.getLuk() + att[i].getLuk();
-      int intel = result.getIntel() + att[i].getIntel();
-
-      result.setDex(dex);
-      result.setSta(sta);
-      result.setStr(str);
-      result.setAgi(agi);
-      result.setLuk(luk);
-      result.setIntel(intel);
+      result.setDex(this.dex + att[i].getDex());
+      result.setSta(this.sta + att[i].getSta());
+      result.setStr(this.str + att[i].getStr());
+      result.setAgi(this.agi + att[i].getAgi());
+      result.setLuk(this.luk + att[i].getLuk());
+      result.setIntel(this.intel + att[i].getIntel());
     }
     return result;
-  }
-
-  public void lvlUp(){
-    int old = this.getLevel();
-    int new_level = old + 1;
-    setLevel(new_level);
-
-    int new_att;
-    new_att = getDex() + 1;
-    setDex(new_att);
-
-    new_att = getSta() + 1;
-    setSta(new_att);
-
-    new_att = getStr() + 1;
-    setStr(new_att);
-
-    new_att = getAgi() + 1;
-    setAgi(new_att);
-
-    new_att = getIntel() + 1;
-    setIntel(new_att);
-
-    new_att = getLuk() + 1;
-    setLuk(new_att);
-
   }
 
   @Override
